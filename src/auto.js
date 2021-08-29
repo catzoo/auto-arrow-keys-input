74be15f4b56f1ae666409a099b706529b5ee1169
catzoo <catzoo7007@gmail.com>
2021-08-29 06:17:56 -0500

    Added auto.js

--- src/auto.js
+++ src/auto.js
@@ -22,3 +22,88 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+var codeReference = {'←': 37, '↑': 38, '→': 39, '↓': 40}
+var active = false
+
+async function push(key) {
+    // keyCode is deprecated, need to modify script.js to use something else
+    var event = new KeyboardEvent('keydown', {'keyCode': key})
+    await new Promise(r => setTimeout(r, 50))
+    document.dispatchEvent(event)
+}
+
+function search(text) {
+    // This uses alpha var from script.js
+    for (let i=0; i<alpha.length; i++) {
+        if (alpha[i].innerHTML === text) {
+            return i + 1
+        }
+    }
+    return null
+}
+
+async function getAlpha(text) {
+    var row = null
+    do {
+        await push(0) // Send nothing to reset all rows
+        row = search(text) // Attempt to find text
+    } while(row === null)
+    return row
+}
+
+async function completeRow(row) {
+    row = document.getElementsByClassName('r' + row)
+    var i = 0
+    var arr = ''
+
+    // Completing the row
+    while(i !== row.length - 1) {
+        // Getting the arrow
+        for (i = row.length - 1; i>=0; i--) {
+            arr = row[i].innerHTML
+            if (arr !== '.') {
+                break
+            }
+        }
+        // Pushing the key
+        await push(codeReference[arr])
+    }
+    // At the end of the row, its completed
+    return true
+}
+
+async function printString(text) {
+    // This uses alphaList var from script.js
+    text = text.toLowerCase()
+
+    for(let i=0; i<text.length; i++) {
+        let a = text[i]
+        if (alphaList.includes(a)) {
+            row = await getAlpha(a)
+            await completeRow(row)
+        }
+    }
+}
+
+async function keyEvent(e) {
+    if (!active) {
+        active = true
+        switch(e.key) {
+            case 'Backspace':
+                await printString('⌫')
+                break
+
+            case 'Control':
+                var text = prompt("Enter a sentence to type")
+                if (text != '' && text !== null)
+                    await printString(text)
+                break
+
+            default:
+                if (e.key.length == 1)
+                    await printString(e.key)
+        }
+        active = false
+    }
+}
+document.addEventListener('keydown', keyEvent)
